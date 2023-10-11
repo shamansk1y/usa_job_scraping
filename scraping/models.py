afe2408437ae1a003a60974441081c1404ef8b0b
shamanskyi <shamansky1225@gmail.com>
2023-10-11 19:11:38 -0400

    upd models

--- scraping/models.py
+++ scraping/models.py
@@ -2,12 +2,20 @@ from django.db import models
 
 
 class Vacancy(models.Model):
-    url = models.CharField(max_length=500)
+    url = models.CharField(max_length=250)
+    url_2 = models.CharField(max_length=250, blank=True, null=True)
     title = models.CharField(max_length=250, verbose_name='Name vacancy')
     company = models.CharField(max_length=250, verbose_name='Company')
     description = models.TextField(verbose_name='Vacancy description')
     search_source = models.CharField(max_length=250, verbose_name='Search source')
     timestamp = models.DateField(auto_now_add=True)
+
+    def full_url(self):
+        if self.url_2:
+            return self.url + self.url_2
+        else:
+            return self.url
+
 
     class Meta:
         verbose_name = 'Job vacancy'
