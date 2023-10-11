755ececb3e092219d011f938ee0204aca66d7cc0
shamanskyi <shamansky1225@gmail.com>
2023-10-11 16:37:50 -0400

    upd pars

--- parsers.py
+++ parsers.py
@@ -35,7 +35,9 @@ chrome_options = webdriver.ChromeOptions()
 # chrome_options.add_argument('--headless')
 chrome_options.add_argument('--disable-dev-shm-usage')
 chrome_options.add_argument('--no-sandbox')
-driver = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), options=chrome_options)
+chrome_driver_path = os.environ.get("CHROMEDRIVER_PATH")
+service = Service(chrome_driver_path)
+driver = webdriver.Chrome(service=service, options=chrome_options)
 
 
 def save_jobs_to_database(jobs):
